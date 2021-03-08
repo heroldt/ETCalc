b34f66eca4586ebf19dd29c0b9a27c88070b13c6
Thomas Herold <thomasherold@outlook.com>
2021-03-08 23:24:48 +0100

    copyright msgbox

--- gui.py
+++ gui.py
@@ -8,6 +8,7 @@ class MainDialog(QtWidgets.QMainWindow):
         self.ui = uic.loadUi("main.ui", self)
         #Slot init
         self.ui.calcButton.clicked.connect(self.onCalcButtonClicked)
+        self.ui.action_about.triggered.connect(self.onAboutClicked)
 
     def clearResultField(self):
         self.ui.label_result.clear()
@@ -18,6 +19,14 @@ class MainDialog(QtWidgets.QMainWindow):
         value = int(self.ui.lineEdit_value.text())
         dev, eval = et_calc.get_closest_erow_value(value,et_calc.e_rows[erow])
         self.ui.label_result.setText("Nearest Value: %.3f (%+.1f%%)" % (eval, (eval/value-1)*100))
+
+    def onAboutClicked(self):
+        msgBox = QtWidgets.QMessageBox()
+        msgBox.setIcon(QtWidgets.QMessageBox.Information)
+        msgBox.setText("Copyright Thomas Herold (c) 2021")
+        msgBox.setWindowTitle("About")
+        msgBox.setStandardButtons(QtWidgets.QMessageBox.Ok)
+        msgBox.exec_()
 
 app = QtWidgets.QApplication(sys.argv)
 dialog = MainDialog()
